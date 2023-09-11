4437b688706fe859d351a45d4d2596bc17d8ea3d
ThomasHascat <dutourne.t@gmail.com>
2023-09-11 17:01:03 +0200

    :fix scss a

--- src/routes/components/Header.jsx
+++ src/routes/components/Header.jsx
@@ -6,8 +6,17 @@ function Header() {
 
         <img className="Logo" alt="logo" ></img>
         <div className="Links-Header">
-            <a className="Link-Acceuil" href="#" target="_blank"> Acceuil</a>
-            <a className="Link-Acceuil" href="#" target="_blank"> A propos</a>
+
+           <div className="link-container">
+                <div className="div-Orange"></div>
+                <a className="Link-Acceuil" href="#" target="_blank">Acceuil</a>
+           </div>
+                
+            
+            <div  className="link-container">
+                <div className="div-Orange propos"></div>
+                <a className="Link-propos" href="#" target="_blank"> A propos</a>
+            </div>
         </div>
 
     </header>
