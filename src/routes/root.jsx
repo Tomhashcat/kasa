4ed18b875ae8ff1ae8d9b6a870b395ec57e741af
ThomasHascat <dutourne.t@gmail.com>
2023-09-11 12:33:30 +0200

    :build React-rooter no error

---
+++ src/routes/root.jsx
@@ -0,0 +1,23 @@
+export default function Root() {
+    return (
+        <>
+
+           
+               
+               
+
+                        <header className="Header">
+
+                            <img className="Logo"></img>
+                            <div className="Links-Header">
+                                <a className="Link-Acceuil" href="#" target="_blank"> Acceuil</a>
+                                <a className="Link-Acceuil" href="#" target="_blank"> A propos</a>
+                            </div>
+
+                        </header>
+                       
+                
+               
+        </>
+    );
+}
