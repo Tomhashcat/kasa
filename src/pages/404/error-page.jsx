a19c31382f4fe38ee5f2e0f5734db71bf85004e1
ThomasHascat <dutourne.t@gmail.com>
2023-09-26 18:55:04 +0200

    :css error page

--- src/pages/404/error-page.jsx
+++ src/pages/404/error-page.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Home from "../Home/Home";
+import "./_Error.scss"
 export default function ErrorPage() {
  
 
   return (
-    <div id="error-page">
+    <div className="error-page">
       <h1>404</h1>
       <p>Oups ! La page que vous demandez n'existe pas.</p>
      <Link to ="/" element={<Home />}>voulez vous revenir à l'acceuil ?</Link>
