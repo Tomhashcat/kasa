5bf0951715a4a532a9c505fe1538f5d3531c06ab
ThomasHascat <dutourne.t@gmail.com>
2023-10-02 09:05:31 +0200

    :fix css

--- src/pages/Fiches/Fiche.jsx
+++ src/pages/Fiches/Fiche.jsx
@@ -15,8 +15,7 @@ export default function FicheLogement() {
   const [pickedAppart, setPickedAppart] = useState();
   useEffect(() => {
     const getData = async () => {
-      const res = await axios.get('/logements.json'); //j'ai préféré utiliser une requète AXIOS pour être prêt à la future mise en place de l'API
-      const picked = res.data.find(({ id }) => id === params.id);
+      const res = await axios.get('/logements.json');  const picked = res.data.find(({ id }) => id === params.id);
       res.data.map(() => setPickedAppart(picked));
       if (picked === undefined) {
         navigate('/404', { state: { message: "Can't get data" } }); //renvoi vers la page 404 en cas d'URL de logement invalide
@@ -24,7 +23,7 @@ export default function FicheLogement() {
     };
     getData();
     // eslint-disable-next-line
-  }, []); // array vide du useEffect pour ne lancer qu'une seule fois
+  }, []);
   const slidePics = pickedAppart && pickedAppart.pictures;
   const tags = pickedAppart && pickedAppart.tags;
   const equipments = pickedAppart && pickedAppart.equipments;
