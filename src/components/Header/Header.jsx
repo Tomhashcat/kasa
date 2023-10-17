058e03a1f34c4f1480e9c154e3ff324c30760ea6
ThomasHascat <dutourne.t@gmail.com>
2023-10-17 11:08:56 +0200

    :fix active header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import logoImage from '../../assets/images/LOGO.jpg';
 import "./Header.scss";
 
 
+import { useState, useEffect } from 'react';
 
 
 function Header() {
+  const location = useLocation();
+  const [currentPath, setCurrentPath] = useState(location.pathname);
+
+  useEffect(() => {
+    setCurrentPath(location.pathname);
+  }, [location]);
+
   return (
     <header>
-      
       <img src={logoImage} alt="Logo"></img>
       <nav>
-        <Link  to="/">ACCEUIL</Link>
-        <Link to="/Apropos" >A PROPOS</Link>
+        <Link to="/" className={currentPath === '/' ? 'active' : ''}>ACCEUIL</Link>
+        <Link to="/Apropos" className={currentPath === '/Apropos' ? 'active' : ''}>A PROPOS</Link>
       </nav>
     </header>
   );
 }
 
 export default Header;
+
